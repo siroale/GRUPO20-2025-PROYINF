bf4c0177ddf6420e3148c7d16e91d5013667b6fa
Joaquín <dominguezbustosjoaquin@gmail.com>
2024-09-30 20:09:10 -0300

    Pequeños cambios en diseño

--- php/visualizacion.php
+++ php/visualizacion.php
@@ -70,7 +70,7 @@
             <div class="carousel-inner">
                <div class="carousel-item active">
                   <div class="container">
-                     <h1 class="banner_taital">Encuentra en la fundación<br>el apyo que necesitas<br>para innovar</h1>
+                     <h1 class="banner_taital">Encuentra en la fundación<br>el apoyo que necesitas<br>para innovar</h1>
                      <div class="btn_main">
                         <div class="more_bt"><a href="#">Boletines</a></div>
                         <div class="contact_bt"><a href="#">Último boletin</a></div>
@@ -79,7 +79,7 @@
                </div>
                <div class="carousel-item">
                   <div class="container">
-                     <h1 class="banner_taital">Encuentra en la fundación<br>el apyo que necesitas<br>para innovar</h1>
+                     <h1 class="banner_taital">Encuentra en la fundación<br>el apoyo que necesitas<br>para innovar</h1>
                      <div class="btn_main">
                         <div class="more_bt"><a href="#">Boletines</a></div>
                         <div class="contact_bt"><a href="#">Último boletin</a></div>
@@ -88,7 +88,7 @@
                </div>
                <div class="carousel-item">
                   <div class="container">
-                     <h1 class="banner_taital">Encuentra en la fundación<br>el apyo que necesitas<br>para innovar</h1>
+                     <h1 class="banner_taital">Encuentra en la fundación<br>el apoyo que necesitas<br>para innovar</h1>
                      <div class="btn_main">
                         <div class="more_bt"><a href="#">Boletines</a></div>
                         <div class="contact_bt"><a href="#">Último boletin</a></div>
@@ -152,234 +152,12 @@
          </div>
       </div>
       <!-- service section end -->
-      <!-- project section start -->
-      <div class="project_section layout_padding">
-         <div class="container">
-            <h1 class="project_taital">Latest Projects</h1>
-            <div class="project_menu">
-               <ul>
-                  <li class="active"><a href="#">All</a></li>
-                  <li><a href="#">Building</a></li>
-                  <li><a href="#">Commercial</a></li>
-                  <li><a href="#">Interior Design</a></li>
-                  <li><a href="#">Residential</a></li>
-               </ul>
-            </div>
-         </div>
-         <div class="project_section_2 layout_padding">
-            <div class="container-fluid">
-               <div class="row">
-                  <div class="col-sm-3">
-                     <div><img src="../images/img-3.png" class="image_3"></div>
-                  </div>
-                  <div class="col-sm-3">
-                     <div><img src="../images/img-4.png" class="image_3"></div>
-                  </div>
-                  <div class="col-sm-3">
-                     <div><img src="../images/img-5.png" class="image_3"></div>
-                  </div>
-                  <div class="col-sm-3">
-                     <div><img src="../images/img-6.png" class="image_3"></div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- project section end -->
-      <!-- philosophy section start -->
-      <div class="philosophy_section layout_padding">
-         <div class="container">
-            <h1 class="philosophy_taital">Our Philosophy</h1>
-            <p class="philosophy_text">It is a long established fact that a reader will be distracted by the readable</p>
-         </div>
-         <div class="philosophy_section_2 layout_padding">
-            <div class="container-fluid">
-               <div class="row">
-                  <div class="col-md-6 padding_0">
-                     <div class="philosophy_box_main">
-                        <div class="philosophy_box">
-                           <div class="philosophy_left">
-                              <h1 class="number_text">01</h1>
-                           </div>
-                           <div class="philosophy_right">
-                              <h3 class="quick_text">Quick in Response</h3>
-                           </div>
-                        </div>
-                        <div class="philosophy_box">
-                           <div class="philosophy_left">
-                              <h1 class="number_text">02</h1>
-                           </div>
-                           <div class="philosophy_right">
-                              <h3 class="quick_text">Satisfaction Guarantee</h3>
-                           </div>
-                        </div>
-                        <div class="philosophy_box">
-                           <div class="philosophy_left">
-                              <h1 class="number_text">03</h1>
-                           </div>
-                           <div class="philosophy_right">
-                              <h3 class="quick_text">Expert & Professional</h3>
-                           </div>
-                        </div>
-                        <div class="philosophy_box">
-                           <div class="philosophy_left">
-                              <h1 class="number_text">04</h1>
-                           </div>
-                           <div class="philosophy_right">
-                              <h3 class="quick_text">Professional Approach</h3>
-                           </div>
-                        </div>
-                        <div class="philosophy_box">
-                           <div class="philosophy_left">
-                              <h1 class="number_text">05</h1>
-                           </div>
-                           <div class="philosophy_right">
-                              <h3 class="quick_text">High Quality Work</h3>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-md-6 padding_0">
-                     <div class="image_7"><img src="../images/img-7.png"></div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- philosophy section end -->
-      <!-- project section start -->
-      <div class="project_section_3 layout_padding">
-         <div class="container">
-            <h1 class="project_taital">Our Features Projects</h1>
-            <p class="philosophy_text">It is a long established fact that a reader will be distracted by the readable</p>
-         </div>
-         <div class="project_section_2 layout_padding">
-            <div id="my_slider" class="carousel slide" data-ride="carousel">
-               <div class="carousel-inner">
-                  <div class="carousel-item active">
-                     <div class="container">
-                        <div class="row">
-                           <div class="col-sm-4">
-                              <div>
-                                 <img src="../images/img-4.png" class="image_6">
-                                 <h4 class="reader_text">Reader will be <br>distracted by the readable</h4>
-                              </div>
-                           </div>
-                           <div class="col-sm-4">
-                              <div>
-                                 <img src="../images/img-5.png" class="image_6">
-                                 <h4 class="reader_text">Reader will be <br>distracted by the readable</h4>
-                              </div>
-                           </div>
-                           <div class="col-sm-4">
-                              <div>
-                                 <img src="../images/img-6.png" class="image_6">
-                                 <h4 class="reader_text">Reader will be <br>distracted by the readable</h4>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="carousel-item">
-                     <div class="container">
-                        <div class="row">
-                           <div class="col-sm-4">
-                              <div>
-                                 <img src="../images/img-4.png" class="image_6">
-                                 <h4 class="reader_text">Reader will be <br>distracted by the readable</h4>
-                              </div>
-                           </div>
-                           <div class="col-sm-4">
-                              <div>
-                                 <img src="../images/img-5.png" class="image_6">
-                                 <h4 class="reader_text">Reader will be <br>distracted by the readable</h4>
-                              </div>
-                           </div>
-                           <div class="col-sm-4">
-                              <div>
-                                 <img src="../images/img-6.png" class="image_6">
-                                 <h4 class="reader_text">Reader will be <br>distracted by the readable</h4>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="carousel-item">
-                     <div class="container">
-                        <div class="row">
-                           <div class="col-sm-4">
-                              <div>
-                                 <img src="../images/img-4.png" class="image_6">
-                                 <h4 class="reader_text">Reader will be <br>distracted by the readable</h4>
-                              </div>
-                           </div>
-                           <div class="col-sm-4">
-                              <div>
-                                 <img src="../images/img-5.png" class="image_6">
-                                 <h4 class="reader_text">Reader will be <br>distracted by the readable</h4>
-                              </div>
-                           </div>
-                           <div class="col-sm-4">
-                              <div>
-                                 <img src="../images/img-6.png" class="image_6">
-                                 <h4 class="reader_text">Reader will be <br>distracted by the readable</h4>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <a class="carousel-control-prev" href="#my_slider" role="button" data-slide="next">
-               <i class="fa fa-arrow-left"></i>
-               </a>
-               <a class="carousel-control-next" href="#my_slider" role="button" data-slide="next">
-               <i class="fa fa-arrow-right"></i>
-               </a>
-            </div>
-         </div>
-      </div>
-      <!-- project section end -->
-      <!-- contact section start -->
-      <div class="contact_section layout_padding">
-         <div class="container">
-            <h1 class="contact_taital">Get In Touch</h1>
-         </div>
-         <div class="contact_section_2 layout_padding">
-            <div class="container">
-               <div class="mail_section">
-                  <div class="row">
-                     <div class="col-md-6">
-                        <input type="" class="input_text" placeholder="Name" name="Name">
-                     </div>
-                     <div class="col-md-6">
-                        <input type="" class="input_text" placeholder="Email" name="Email">
-                     </div>
-                  </div>
-                  <input type="" class="input_text" placeholder="Phone Number" name="Phone Number">
-                  <textarea name="comment" class="massage_box" form="usrform">Message</textarea>
-                  <div class="send_bt">
-                     <div class="send_text"><a href="#">SEND</a></div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- contact section end -->
-      <!-- newslatter section start -->
-      <div class="newslatter_section layout_padding">
-         <div class="container">
-            <div class="newslatter_main">
-               <h1 class="looking_text">Do you looking for Architecture design expertise?</h1>
-               <div class="quote_bt"><a href="#">Get A Quote</a></div>
-            </div>
-         </div>
-      </div>
-      <!-- newslatter section end -->
+      
       <!-- client section start -->
       <div class="client_section layout_padding">
          <div class="container">
-            <h1 class="client_taital">What Is say clients </h1>
-            <p class="client_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum </p>
+            <h1 class="client_taital">Lo que dicen nuestros colaboradores</h1>
+            <p class="client_text">Opiniones serias y sinceras sobre nuestro trabajo</p>
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                <ol class="carousel-indicators">
                   <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -391,12 +169,12 @@
                      <div class="client_section_2 layout_padding">
                         <div class="client_main">
                            <div class="client_left">
-                              <div class="client_img"><img src="../images/client-img.png"></div>
-                              <h4 class="henrry_name">Henrry Jo</h4>
-                              <h6 class="henrry_text">( long established )</h6>
+                              <div class="client_img"><img src="../images/tremendo_nucleo.png" width="200" height="200"></div>
+                              <h4 class="henrry_name">Viktor Tapia</h4>
+                              <h6 class="henrry_text">( Plata Infinita )</h6>
                            </div>
                            <div class="client_right">
-                              <p class="lorem_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look </p>
+                              <p class="lorem_text">Que iphone me compro? uno de un palo y medio o uno de un palo setecientos, NO IMPORTA, yo tengo mucha plata </p>
                            </div>
                         </div>
                      </div>
@@ -405,12 +183,12 @@
                      <div class="client_section_2 layout_padding">
                         <div class="client_main">
                            <div class="client_left">
-                              <div class="client_img"><img src="../images/client-img.png"></div>
-                              <h4 class="henrry_name">Henrry Jo</h4>
-                              <h6 class="henrry_text">( long established )</h6>
+                              <div class="client_img"><img src="../images/colaborador-2.gif" width="200" height="200"></div>
+                              <h4 class="henrry_name">Profe de OSI</h4>
+                              <h6 class="henrry_text">( UML LOVER )</h6>
                            </div>
                            <div class="client_right">
-                              <p class="lorem_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look </p>
+                              <p class="lorem_text">Amamons OSI</p>
                            </div>
                         </div>
                      </div>
@@ -419,12 +197,12 @@
                      <div class="client_section_2 layout_padding">
                         <div class="client_main">
                            <div class="client_left">
-                              <div class="client_img"><img src="../images/client-img.png"></div>
-                              <h4 class="henrry_name">Henrry Jo</h4>
-                              <h6 class="henrry_text">( long established )</h6>
+                              <div class="client_img"><img src="../images/colaborador-3.png" width="200" height="200"></div>
+                              <h4 class="henrry_name">Ruggiero</h4>
+                              <h6 class="henrry_text">( Gritón )</h6>
                            </div>
                            <div class="client_right">
-                              <p class="lorem_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look </p>
+                              <p class="lorem_text">Si tienen dudas vuelvan al kinder o vayan a preguntarme afuera que estoy fumando. </p>
                            </div>
                         </div>
                      </div>
@@ -439,13 +217,13 @@
          <div class="container">
             <div class="row">
                <div class="col-lg-4 col-sm-12">
-                  <div class="map_text"><a href="#"><img src="../images/map-icon.png" class="image_main"><span class="padding_left_10">No.123 Chalingt Gates, Supper market New York</span></a></div>
+                  <div class="map_text"><a href="#"><img src="../images/map-icon.png" class="image_main"><span class="padding_left_10">Loreley 1582, La Reina, Santiago</span></a></div>
                </div>
                <div class="col-lg-4 col-sm-12">
-                  <div class="map_text"><a href="#"><img src="../images/call-icon.png" class="image_main"><span class="padding_left_10">(+71 7986543234)</span></a></div>
+                  <div class="map_text"><a href="#"><img src="../images/call-icon.png" class="image_main"><span class="padding_left_10">+562 2431 3000</span></a></div>
                </div>
                <div class="col-lg-4 col-sm-12">
-                  <div class="map_text"><a href="#"><img src="../images/mail-icon.png" class="image_main"><span class="padding_left_10">demo@gmail.com</span></a></div>
+                  <div class="map_text"><a href="#"><img src="../images/mail-icon.png" class="image_main"><span class="padding_left_10">contacto@fia.cl</span></a></div>
                </div>
             </div>
          </div>
@@ -454,7 +232,7 @@
       <!-- copyright section start -->
       <div class="copyright_section">
          <div class="container">
-            <p class="copyright_text">Copyright 2019 All Right Reserved By.<a href="https://html.design"> Free  html Templates</p>
+            <p class="copyright_text">Copyright 2022 Todos los derechos reservados sansanews.</p>
          </div>
       </div>
       <!-- copyright section end -->    
