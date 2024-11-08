6e3d667444a69ad3922d9f0b9a3874bc44c6e4f4
Joaquín <dominguezbustosjoaquin@gmail.com>
2024-11-08 20:56:13 -0300

    Boton Google

--- src/php/landing.php
+++ src/php/landing.php
@@ -41,7 +41,7 @@
             <button type="submit" class="sign-in_btn">
                 <span>Ingresar</span>
             </button>
-            <a href="login_google.php" title="Sign In" type="submit" class="sign-in_ggl">
+            <a href="login_google.php" type="submit" class="sign-in_ggl">
                 <svg height="18" width="18" viewBox="0 0 32 32" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
                 <defs>
                     <path d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" id="A"></path>
@@ -56,7 +56,7 @@
                     <path fill="#4285f4" clip-path="url(#B)" d="M48 48L17 24l-4-3 35-10z"></path>
                 </g>
                 </svg>
-                <span>Sign In with Google</span>
+                <span>Ingresar con Google</span>
             </a>
 
             <div class="separator">
