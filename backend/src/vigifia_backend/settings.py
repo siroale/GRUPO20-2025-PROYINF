af0a39ab8f171924b56ad26176b834d529205070
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2025-04-18 13:17:31 -0400

    configurar bien django

--- backend/src/vigifia_backend/settings.py
+++ backend/src/vigifia_backend/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.getenv('SECRET_KEY')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
