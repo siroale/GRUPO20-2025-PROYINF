a3cc0c898019fbc2057aa1acadad79f932971873
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2025-04-26 10:17:25 -0400

    fix: eliminar importaciones no necesarias

--- frontend/src/components/pages/Register.tsx
+++ frontend/src/components/pages/Register.tsx
@@ -1,6 +1,4 @@
 // src/components/pages/Register.tsx
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
 import RegisterForm from "@/components/layout/RegisterForm"
 
 const Register = () => {
